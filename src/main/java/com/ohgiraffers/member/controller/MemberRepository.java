6aec6a5851c844bd008ec5490b687e6976ffb1d5
MANTISKYU <asdaron44@gmail.com>
2024-06-17 19:11:32 +0900

    시험시험

--- src/main/java/com/ohgiraffers/member/controller/MemberRepository.java
+++ src/main/java/com/ohgiraffers/member/controller/MemberRepository.java
@@ -28,8 +28,8 @@ public class MemberRepository {
 
         for(Member m : memberList) {
 
-            System.out.println("상품가격이 " + price + "원일 경우 " + "이름이 " + m.getName() + "인 사람의" + " 등급은 " + m.getGrade() + "이고 " + "포인트는 " + m.getPoint()
-            + "이며 " +m.calculateInterest(price)  + " 원을 할인받아 " + (price - (m.calculateInterest(price))) + "원에 물품을 구입할 수 있습니다");
+            System.out.println("상품가격이 " + price + "원일 경우 " + "이름이 " + m.getName() + "인 사람의" + " 등급은 " + m.getGrade() + "이고 " + m.getPoint()
+            + "포인트를 보유중이며 " +m.calculateInterest(price)  + " 원을 할인받아 " + (price - (m.calculateInterest(price))) + "원에 물품을 구입할 수 있습니다");
 
 
         }
